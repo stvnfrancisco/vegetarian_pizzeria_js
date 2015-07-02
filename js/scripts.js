4bdae7073f3134f16f4719107a76fa670a0ef8bb
steven francisco <stvnfrancisco@yahoo.com>
2015-07-02 11:39:25 -0700

    passing specs for Pizza.addTopping and Pizza.findCost function

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,19 @@ function Pizza(size, toppings, cost){
   this.toppings = []
   this.cost = 0
 }
+
+Pizza.addTopping = function(topping){
+  (this.toppings).push(topping);
+}
+
+Pizza.findCost = function(){
+  if (this.size == "Small"){
+    this.cost += 8;
+  } else if (this.size == "Medium"){
+    this.cost += 10;
+  } else if (this.size == "Large"){
+    this.cost += 12;
+  }
+  this.cost += (this.toppings.length);
+  return this.cost;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,14 @@ describe('Pizza', function(){
     var testPizza = new Pizza()
     expect(testPizza.cost).to.equal(0);
   });
+
+  it("starts with no toppings", function(){
+    var testPizza = new Pizza()
+    expect(testPizza.toppings).to.eql([]);
+  });
+
+  it("lets the user select a size", function(){
+    var testPizza = new Pizza("large")
+    expect(testPizza.size).to.equal("large");
+  });
 });
